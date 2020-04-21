54f4fcf2b504d9d2fb6873306e4c5d2758f1e756
zps <2235969352@qq.com>
2020-04-21 15:48:23 +0800

    添加备注

--- xml_demo/src/main/java/com/zps/xml_demo/XmlDemoApplication.java
+++ xml_demo/src/main/java/com/zps/xml_demo/XmlDemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XmlDemoApplication {
 
 
+    //启动类
     public static void main(String[] args) {
         SpringApplication.run(XmlDemoApplication.class, args);
     }
